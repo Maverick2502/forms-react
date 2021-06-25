ff999b21ccc82ea760fbe242ab93d28fd931c844
Nurullo <khuwaidullaev@gmail.com>
2021-06-25 05:05:22 +0500

    Refactoring and Deriving States

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,44 +1,61 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 
-const SimpleInput = (props) => {
+function SimpleInput(props) {
   const [enteredName, setEnteredName] = useState("");
-  const nameInputRef = useRef();
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+
+  const enteredNameIsValid = enteredName.trim() !== "";
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
 
   function nameInputChangeHandler(e) {
     setEnteredName(e.target.value);
   }
 
+  function nameInputBlurHandler(e) {
+    setEnteredNameTouched(true);
+  }
+
   function formSubmissionHandler(e) {
     e.preventDefault();
-    if (enteredName.trim() === "") {
+
+    setEnteredNameTouched(true);
+
+    if (!enteredNameIsValid) {
       return;
     }
 
-    const enteredValue = nameInputRef.current.value;
-    console.log(enteredValue);
+    console.log(enteredName);
 
-    // Resetting entered value not possible with useRef();
+    // Resetting entered value not good with useRef();
     setEnteredName("");
+    setEnteredNameTouched(false);
   }
+
+  const nameInputClasses = nameInputIsInvalid
+    ? "form-control invalid"
+    : "form-control ";
 
   return (
     <form onSubmit={formSubmissionHandler}>
-      <div className="form-control">
+      <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
         <input
-          ref={nameInputRef}
           type="text"
           id="name"
           onChange={nameInputChangeHandler}
+          onBlur={nameInputBlurHandler}
           // Resetting entered value
           value={enteredName}
         />
+        {nameInputIsInvalid && (
+          <p className="error-text">Name must not be empty</p>
+        )}
       </div>
       <div className="form-actions">
         <button>Submit</button>
       </div>
     </form>
   );
-};
+}
 
 export default SimpleInput;
